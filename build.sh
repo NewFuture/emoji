7f72d188a1745eafa15efbe817ee475d1d7ee314
NewFuture <new_future@msn.cn>
2016-08-30 18:44:55 +0800

    :turtle:  commit hook :eyes:

--- build.sh
+++ build.sh
@@ -1,11 +1,49 @@
 #!/usr/bin/env bash 
-OUTPUT='e.sh' 
+
+
+# BUILD a command to random an github emoji
+OUTPUT=$1'e.sh' 
 
 cat > $OUTPUT <<'BUILD' 
 #!/usr/bin/env bash
-shuf -n1<<'EMOJI'
+shuf -n1<<'EMOJIS'
 BUILD
 
 cat emoji.md>>$OUTPUT 
 
-echo "EMOJI">>$OUTPUT 
+echo "EMOJIS">>$OUTPUT 
+
+chmod +x $OUTPUT
+
+
+
+
+# BUILD a command to random an github emoji
+
+OUTPUT=$1'c.sh' 
+
+cat > $OUTPUT <<'BUILD'
+#!/usr/bin/env bash
+COMMIT_MSG='.git/hooks/commit-msg'
+cat >$COMMIT_MSG<<'COMMIT'
+#!/bin/sh
+EMOJI=$(shuf -n1<<'EMOJIS'
+BUILD
+
+cat emoji.md>>$OUTPUT 
+
+cat >> $OUTPUT <<'BUILD'
+
+EMOJIS
+);
+BUILD
+
+cat 'commit-msg'>>$OUTPUT
+
+cat >> $OUTPUT <<'BUILD'
+
+COMMIT
+chmod +x $COMMIT_MSG
+BUILD
+
+chmod +x $OUTPUT
