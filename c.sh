dc7406ac1107dbc75dbb60fbda93d724fce02fcc
From-Travis-CI <github@newfuture.cc>
2016-08-31 02:14:50 +0000

    :ab: Auto build from master :suspect:

--- c.sh
+++ c.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#the scipt auto add the commit-msg hook of git
 
 # path of <commit-msg>
 COMMIT_MSG='.git/hooks/commit-msg'
@@ -896,18 +897,21 @@ EMOJI=$({{SHUF_ONE}}<<'EMOJIS'
 :zzz:
 EMOJIS
 );
-
-echo "$EMOJI $(cat $1)" > $1
+echo "$EMOJI $(cat $1)" >$1
 COMMIT
 
+PERL_SHUF="perl -MList::Util=shuffle -e 'print @{[shuffle<>]}[0];'";
+# replace the shuffle method in the <commit-msg>
 if [ "$(uname)" == "Darwin" ]; then 
-#MAC
-    SHUFONE="perl -MList::Util=shuffle -e 'print @{[shuffle<>]}[0];'";
+#MAC use perl shuffle
+    sed -i '' "s/{{SHUF_ONE}}/${PERL_SHUF}/g" "$COMMIT_MSG"
 else 
-# LINUX
-    SHUFONE='shuf -n1';
+# LINUX use the shuf preferentially
+    SHUF='shuf -n1';
+    command -v shuf >/dev/null 2>&1 || SHUF=$PERL_SHUF;
+    sed -i'' "s/{{SHUF_ONE}}/${SHUF}/g" "$COMMIT_MSG"
 fi;
 
-# replace the shuf method in the <commit-msg>
-sed -i'' "s/{{SHUF_ONE}}/${SHUFONE}/g" "$COMMIT_MSG"
 chmod +x "$COMMIT_MSG"
+
+echo "^_^ Well done! The Github Emojis hook of commit-msg is ready! :)"
