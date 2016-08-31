85e2cc68e31d3a3de14a246b3fdf55c5d071bdf6
NewFuture <new_future@msn.cn>
2016-08-31 10:09:53 +0800

    :hotel: mac and tips :joy:

--- creater.sh
+++ creater.sh
@@ -2,6 +2,7 @@
 ##:: the template of the create scipt to build commit-msg hook
 ##:: {{}} is template vars
 ##:: commmet start with ##::
+#the scipt auto add the commit-msg hook of git
 
 # path of <commit-msg>
 COMMIT_MSG='.git/hooks/commit-msg'
@@ -10,14 +11,18 @@ cat >"$COMMIT_MSG"<<'COMMIT'
 {{COMMITMSG}}
 COMMIT
 
+PERL_SHUF="perl -MList::Util=shuffle -e 'print @{[shuffle<>]}[0];'";
+# replace the shuffle method in the <commit-msg>
 if [ "$(uname)" == "Darwin" ]; then 
-#MAC
-    SHUFONE="perl -MList::Util=shuffle -e 'print @{[shuffle<>]}[0];'";
+#MAC use perl shuffle
+    sed -i '' "s/{{SHUF_ONE}}/${PERL_SHUF}/g" "$COMMIT_MSG"
 else 
-# LINUX
-    SHUFONE='shuf -n1';
+# LINUX use the shuf preferentially
+    SHUF='shuf -n1';
+    command -v shuf >/dev/null 2>&1 || SHUF=$PERL_SHUF;
+    sed -i'' "s/{{SHUF_ONE}}/${SHUF}/g" "$COMMIT_MSG"
 fi;
 
-# replace the shuf method in the <commit-msg>
-sed -i'' "s/{{SHUF_ONE}}/${SHUFONE}/g" "$COMMIT_MSG"
 chmod +x "$COMMIT_MSG"
+
+echo "^_^ Well done! The Github Emojis hook of commit-msg is ready! :)"
